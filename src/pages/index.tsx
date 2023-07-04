d0ee6d6abd8b7fd721e8f2c52cc38766dae9cfde
s1f102100793 <s1f102100793@iniad.org>
2023-07-04 20:59:47 +0900

    グレーの9*9作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,6 +18,7 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
   const bombcount = 10;
+  const newBoard: number[][] = JSON.parse(JSON.stringify(userInputs));
 
   const [bombMap, setBombMap] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -30,6 +31,7 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+
   const isPlaying = userInputs.some((row) => row.some((input) => input !== 0));
   const isFailing = userInputs.some((row, y) =>
     row.some((input, x) => input === 1 && bombMap[y][x] === 1)
@@ -40,7 +42,57 @@ const Home = () => {
   // 9 -> 石+はてな
   // 10 -> 石+旗
   // 11 -> ボムセル
-  return <div className={styles.container}>hoge</div>;
+
+  // borad = userInputs + bombMap
+
+  const directions: number[][] = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, 1],
+    [1, 0],
+    [1, -1],
+  ];
+
+  //再帰関数で0を押したら全部表示を切り替える
+  const board = (x: number, y: number) => {
+    if (newBoard[y][x] === 0) {
+      for (const w of directions) {
+        newBoard[y + w[0]][x + w[1]] === 0;
+      }
+    }
+  };
+
+  let gameClick = 0;
+
+  const onClick = (x: number, y: number) => {
+    if (gameClick === 0) {
+      gameClick++;
+      if (userInputs[y][x] === 0) {
+      }
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.board}>
+        {userInputs.map((row, y) =>
+          row.map((color, x) => (
+            <div className={styles.cell} key={`${x}-${y}`} onClick={() => onClick(x, y)}>
+              {color !== 0 && (
+                <div
+                  className={styles.hoge}
+                  style={{ background: color === 3 ? '#adff2f' : color === 1 ? '#000' : '#fff' }}
+                />
+              )}
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
