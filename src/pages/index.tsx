9e274fb5c1db0c6d9461822edf5d5af947e92777
s1f102100793 <s1f102100793@iniad.org>
2023-07-08 15:34:38 +0900

    userInputsがすべて1になる以外できた

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import styles from './index.module.css';
 
-const Home = () => {
+const Home = (y: number, x: number) => {
   // 0 -> 見クリック
   // 1 -> 左クリック
   // 2 -> はてな
@@ -31,6 +31,19 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+
+  const board = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
+
   const newBomb: number[][] = JSON.parse(JSON.stringify(bombMap));
 
   const isPlaying = userInputs.some((row) => row.some((input) => input !== 0));
@@ -58,27 +71,48 @@ const Home = () => {
   ];
 
   //再帰関数で0を押したら全部表示を切り替える
-  const board = (y: number, x: number) => {
-    if (bombMap[y][x] === 11) {
-      alert('Gameover');
-    } else {
-      if (bombMap[y][x] === 0) {
-        for (const w of directions) {
-          if (
-            newBoard[y + w[0]] !== undefined &&
-            newBoard[x + w[1]] !== undefined &&
-            userInputs[y + w[0]][x + w[1]] !== 1
-          ) {
-            userInputs[y + w[0]][x + w[1]] = 1;
-            board(y + w[0], x + w[1]);
-          }
+  const clickBoard = (y: number, x: number) => {
+    if (board[y][x] === 0) {
+      for (const w of directions) {
+        if (
+          newBoard[y + w[0]] !== undefined &&
+          newBoard[x + w[1]] !== undefined &&
+          userInputs[y + w[0]][x + w[1]] !== 1
+        ) {
+          userInputs[y + w[0]][x + w[1]] = 1;
+          clickBoard(y + w[0], x + w[1]);
         }
-      } else {
-        userInputs[y][x] = 1;
-        console.log('elseが機能している');
       }
+    } else {
+      userInputs[y][x] = 1;
+      console.log('elseが機能している');
     }
   };
+
+  for (let y = 0; y < 9; y++) {
+    for (let x = 0; x < 9; x++) {
+      for (const w of directions) {
+        if (board[y + w[0]] !== undefined && bombMap[y + w[0]][x + w[1]] === 1) {
+          if (bombMap[y][x] !== 1) {
+            board[y][x]++;
+          } else {
+            continue;
+          }
+        }
+      }
+    }
+  }
+
+  console.log('board');
+  console.table(board);
+
+  // if (userInputs[y][x] === 1) {
+  //   if (bombMap[y][x] === 1) {
+  //     alert('Gameover');
+  //   } else {
+  //     clickBoard(y, x);
+  //   }
+  // }
 
   let gameClick = 0;
 
@@ -91,39 +125,27 @@ const Home = () => {
       while (realbomb < bombcount) {
         const bombX = Math.floor(Math.random() * bombMap.length);
         const bombY = Math.floor(Math.random() * bombMap[0].length);
-        if (bombMap[bombY][bombX] !== 11 && `${bombY}${bombX}` !== `${y}${x}`) {
+        if (bombMap[bombY][bombX] !== 1 && `${bombY}${bombX}` !== `${y}${x}`) {
           const BombMapCopy = [...bombMap];
-          BombMapCopy[bombY][bombX] = 11;
+          BombMapCopy[bombY][bombX] = 1;
           setBombMap(BombMapCopy);
           realbomb++;
         }
       }
 
-      for (let y = 0; y < 9; y++) {
-        for (let x = 0; x < 9; x++) {
-          for (const w of directions) {
-            if (bombMap[y + w[0]] !== undefined && bombMap[y + w[0]][x + w[1]] === 11) {
-              if (bombMap[y][x] !== 11) {
-                bombMap[y][x]++;
-              } else {
-                continue;
-              }
-            }
-          }
-        }
-      }
-
-      board(y, x);
+      clickBoard(y, x);
+      console.log('bombMap');
       console.table(bombMap);
       setBombMap(bombMap);
       console.log('一クリ目', y, x);
     } else {
       // 2クリ目以降
       // クリックの関数を設置（再帰関数のやつ）
-      board(y, x);
+      clickBoard(y, x);
       console.log('2クリ目以降', y, x);
     }
     userInputs[y][x] = 1;
+    console.log('userInputs');
     console.table(userInputs);
     setuserInputs(userInputs);
   };
@@ -133,7 +155,7 @@ const Home = () => {
   return (
     <div className={styles.container}>
       <div className={styles.board}>
-        {userInputs.map((row, y) =>
+        {board.map((row, y) =>
           row.map((color, x) => (
             <div className={styles.cell} key={`${y}-${x}`} onClick={() => onClick(y, x)} />
           ))
