cba3385f4c37eb0b40d9219cf4ab9f78c50a2f57
s1f102100793 <s1f102100793@iniad.org>
2023-07-11 18:43:13 +0900

    ミット

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import styles from './index.module.css';
-
 const Home = () => {
   // 0 -> 見クリック
   // 1 -> 左クリック
@@ -19,7 +18,6 @@ const Home = () => {
   ]);
   const bombcount = 10;
   const newInputs: (0 | 1 | 2 | 3)[][] = JSON.parse(JSON.stringify(userInputs));
-
   const [bombMap, setBombMap] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -40,7 +38,6 @@ const Home = () => {
     // [1, 0, 0, 0, 0, 0, 0, 0, 1],
     // [1, 1, 1, 1, 1, 1, 1, 1, 1],
   ]);
-
   const board: number[][] = [
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
@@ -53,7 +50,6 @@ const Home = () => {
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
   ];
   // const newBoard = JSON.parse(JSON.stringify(board));
-
   // const isPlaying = userInputs.some((row) => row.some((input) => input !== 0));
   // const isFailing = userInputs.some((row, y) =>
   //   row.some((input, x) => input === 1 && bombMap[y][x] === 1)
@@ -64,9 +60,7 @@ const Home = () => {
   // 9 -> 石+はてな
   // 10 -> 石+旗
   // 11 -> ボムセル
-
   // borad = userInputs + bombMap
-
   const directions: number[][] = [
     [-1, -1],
     [-1, 0],
@@ -77,7 +71,6 @@ const Home = () => {
     [1, -1],
     [0, -1],
   ];
-
   const bombcounts = (y: number, x: number) => {
     let bomb = 0;
     for (const w2 of directions) {
@@ -86,11 +79,17 @@ const Home = () => {
         board[x + w2[1]] !== undefined &&
         bombMap[y + w2[0]][x + w2[1]] === 1 &&
         userInputs[y + w2[0]][x + w2[1]] === 0 &&
-        board[y + w2[0]][x + w2[1]] === -1 &&
-        board[y + w2[0]][x + w2[1]] !== 0
+        board[y + w2[0]][x + w2[1]] === -1
       ) {
         bomb++;
         // break;
+      } else if (
+        board[y + w2[0]] !== undefined &&
+        board[x + w2[1]] !== undefined &&
+        userInputs[y][x] === 1 &&
+        bombMap[y][x] === 1
+      ) {
+        bomb++;
       }
     }
     if (bomb === 0) {
@@ -99,33 +98,41 @@ const Home = () => {
         if (
           board[y + w1[0]] !== undefined &&
           board[x + w1[1]] !== undefined &&
-          board[y + w1[0]][x + w1[1]] === -1
+          board[y + w1[0]][x + w1[1]] === -1 &&
+          board[y][x] !== 11
         ) {
           bombcounts(y + w1[0], x + w1[1]);
         }
       }
     } else {
-      console.log(bomb);
       board[y][x] = bomb;
     }
   };
-
   for (let y = 0; y < 9; y++) {
     for (let x = 0; x < 9; x++) {
-      // 再帰関数;
       if (userInputs[y][x] === 1) {
-        bombcounts(y, x);
+        if (bombMap[y][x] === 1) {
+          // alert('Gameover2');
+          for (let y = 0; y < 9; y++) {
+            for (let x = 0; x < 9; x++) {
+              if (bombMap[y][x] === 1) {
+                board[y][x] = 11;
+              }
+            }
+          }
+          break;
+        } else {
+          bombcounts(y, x);
+        }
       }
     }
   }
-
   console.log('board');
   console.table(board);
   console.log('bombMap');
   console.table(bombMap);
   console.log('userInputs');
   console.table(userInputs);
-
   let bombbb = 0;
   for (let y = 0; y < 9; y++) {
     for (let x = 0; x < 9; x++) {
@@ -135,33 +142,30 @@ const Home = () => {
     }
   }
   const onClick = (y: number, x: number) => {
-    if (bombMap[y][x] === 1) {
-      alert('Gameover');
-    } else {
-      if (bombbb === 0) {
-        // 一クリ目
-        // ランダムに10個ボムをクリックしたマス以外で作成
-        let realbomb = 0;
-        while (realbomb < bombcount) {
-          const bombX = Math.floor(Math.random() * bombMap.length);
-          const bombY = Math.floor(Math.random() * bombMap[0].length);
-          if (bombMap[bombY][bombX] !== 1 && `${bombY}${bombX}` !== `${y}${x}`) {
-            const BombMapCopy = [...bombMap];
-            BombMapCopy[bombY][bombX] = 1;
-            realbomb++;
-            setBombMap(BombMapCopy);
-          }
+    // if (bombMap[y][x] === 1) {
+    //   alert('Gameover');
+    // } else {
+    if (bombbb === 0) {
+      // 一クリ目
+      // ランダムに10個ボムをクリックしたマス以外で作成
+      let realbomb = 0;
+      while (realbomb < bombcount) {
+        const bombX = Math.floor(Math.random() * bombMap.length);
+        const bombY = Math.floor(Math.random() * bombMap[0].length);
+        if (bombMap[bombY][bombX] !== 1 && `${bombY}${bombX}` !== `${y}${x}`) {
+          const BombMapCopy = [...bombMap];
+          BombMapCopy[bombY][bombX] = 1;
+          realbomb++;
+          setBombMap(BombMapCopy);
         }
-        console.log('一クリ目', y, x);
       }
-
-      newInputs[y][x] = 1;
-      setuserInputs(newInputs);
+      console.log('一クリ目', y, x);
     }
+    newInputs[y][x] = 1;
+    setuserInputs(newInputs);
+    // }
   };
-
   // setBombMap(newBomb);
-
   return (
     <div className={styles.container}>
       <div className={styles.board}>
@@ -169,7 +173,7 @@ const Home = () => {
           row.map((number, x) => (
             <div
               className={number === -1 ? styles.stone : styles.number}
-              style={number >= 0 ? { backgroundPosition: `${(number - 1) * -80}px 0` } : {}}
+              style={number >= 0 ? { backgroundPosition: `${(number - 1) * -75.86}px 0` } : {}}
               key={`${y}-${x}`}
               onClick={() => onClick(y, x)}
             />
@@ -179,5 +183,4 @@ const Home = () => {
     </div>
   );
 };
-
 export default Home;
