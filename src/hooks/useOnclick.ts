536ec2779cdb3e056e3a5f2590fac351f4a7dd65
s1f102100793 <s1f102100793@iniad.org>
2023-07-15 15:47:57 +0900

    いらないファイルを消去

--- src/hooks/useOnclick.ts
+++
@@ -1,6 +0,0 @@
-import { useBoard } from './useBoard';
-
-export const useOnclick = () => {
-  const { userInputs, bombMap, setuserInputs, setBombMap, newInputs, bombbb, setTimer } =
-    useBoard();
-};
